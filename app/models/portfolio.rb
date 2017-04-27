27ad0c902427b0f2d8e6d09db4a248c58a526687
Jiří Čech <cechtech@pc.local>
2017-04-27 20:16:13 +0200

    Implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,12 @@
 class Portfolio < ApplicationRecord
   # Validates table records
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # Creates function with query for use elsewhere
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  # Another method .uby_on_rails_portfolio_items available  query
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}",
+  )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Aenean fermentum risus id tortor. Nullam at arcu a est sollicitudin euismod. Fusce aliquam vestibulum ipsum. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nunc tincidunt ante vitae massa. Quisque porta. Nunc auctor. Maecenas sollicitudin. Aliquam id dolor. Aliquam erat volutpat. Phasellus et lorem id felis nonummy placerat. In dapibus augue non sapien. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. In dapibus augue non sapien. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Nunc tincidunt ante vitae massa. Vivamus porttitor turpis ac leo. Nam sed tellus id magna elementum tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Donec quis nibh at felis congue commodo. Vivamus ac leo pretium faucibus. Ut tempus purus at lorem. Nulla non lectus sed nisl molestie malesuada. Donec iaculis gravida nulla. Aliquam erat volutpat. Pellentesque ipsum. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Maecenas lorem. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Aenean fermentum risus id tortor. Nullam at arcu a est sollicitudin euismod. Fusce aliquam vestibulum ipsum. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nunc tincidunt ante vitae massa. Quisque porta. Nunc auctor. Maecenas sollicitudin. Aliquam id dolor. Aliquam erat volutpat. Phasellus et lorem id felis nonummy placerat. In dapibus augue non sapien. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. In dapibus augue non sapien. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Nunc tincidunt ante vitae massa. Vivamus porttitor turpis ac leo. Nam sed tellus id magna elementum tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Donec quis nibh at felis congue commodo. Vivamus ac leo pretium faucibus. Ut tempus purus at lorem. Nulla non lectus sed nisl molestie malesuada. Donec iaculis gravida nulla. Aliquam erat volutpat. Pellentesque ipsum. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Maecenas lorem. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Aenean fermentum risus id tortor. Nullam at arcu a est sollicitudin euismod. Fusce aliquam vestibulum ipsum. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nunc tincidunt ante vitae massa. Quisque porta. Nunc auctor. Maecenas sollicitudin. Aliquam id dolor. Aliquam erat volutpat.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x150"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Aenean fermentum risus id tortor. Nullam at arcu a est sollicitudin euismod. Fusce aliquam vestibulum ipsum. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nunc tincidunt ante vitae massa. Quisque porta. Nunc auctor. Maecenas sollicitudin. Aliquam id dolor. Aliquam erat volutpat.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x150"
