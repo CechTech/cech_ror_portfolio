e5e4c1b463cc9b6c43a69f2681a98f85f933f0f1
Jiří Čech <cechtech@pc.local>
2017-04-28 17:46:17 +0200

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first # .split takes string and splits it into its own arrays by space
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
