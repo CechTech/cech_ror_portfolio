4dc9cb52112fb3eb9170d1a22c66fc87561b7a0f
Jiří Čech <cechtech@pc.local>
2017-06-30 15:03:22 +0200

    Updated file upload forms and refactored placeholder functionality into view

--- app/controllers/concerns/placeholder.rb
+++
@@ -1,8 +0,0 @@
-# Creates concern for placeholder url shortcut
-module Placeholder
-  extend ActiveSupport::Concern
-
-  def self.image_generator(height:, width:)
-    "http://placehold.it/#{height}x#{width}"
-  end
-end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,14 +1,4 @@
 class Skill < ApplicationRecord
-  include Placeholder
   # Validates table records
   validates_presence_of :title, :percent_utilized
-
-  # Initializes defaults after whole code
-  after_initialize :set_defaults
-
-  # Sets defaults for every portfolio item
-  # ||= shortcut for if, if nil adds default, if full skips
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
-  end
 end
