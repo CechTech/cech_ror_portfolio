b10bb3fe47265c41f1f7519a02457bb3cb941bb8
Jiří Čech <cechtech@pc.local>
2017-07-03 12:40:54 +0200

    Added view component for skills

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,15 @@ class PagesController < ApplicationController
   def home
     @posts = Blog.all
     @skills = Skill.all
-    @technologies = Technology.all
   end
 
   def about
+    @skills = Skill.all
   end
 
   def contact
   end
-  
+
   def tech_news
     @tweets = SocialTool.twitter_search
   end
